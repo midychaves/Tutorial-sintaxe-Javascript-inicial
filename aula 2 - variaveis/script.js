7bc848f324a729453b0d6113cb5aa92871362890
midy chaves <midymorena@gmail.com>
2022-06-18 18:54:45 -0300

    tutorial da sintaxe do javascript

---
+++ aula 2 - variaveis/script.js
@@ -0,0 +1,82 @@
+  // tipos primitivos
+
+    //boolean
+    var vOuf = false;
+    console.log(typeof(vOuf));
+
+  // number
+  var numeroQualquer = 1;
+  console.log(typeof(numeroQualquer));
+
+   //string
+  var nome = 'midy';
+  console.log(typeof(nome));
+
+  //como declarar
+  var variavel = 'midy';
+  variavel = 'debinho';
+  console.log(variavel);
+
+  //let variavel2 = 'pretinha';
+  variavel2 = 'betouwe';
+  console.log(variavel2);
+
+ // const constante = 'midy';
+  constante = 'debinho';
+  console.log(constante);
+  
+  var escopoglobal = 'global';
+  console.log(escopoglobal);
+
+  function escopolocal() {
+    let escopolocalinterno = 'local'; 
+    console.log(escopolocalinterno);
+  }
+  escopolocal();
+
+  //atribuiçao
+  var atribuiçao = "midy";
+
+  //comparaçao
+  var comparaçao ='0'== 0;
+  console.log(comparaçao);
+
+  //comparaçaoIdentica 
+  var comparaçaoIdentica = '0' === 0;
+  console.log(comparaçaoIdentica);
+
+  //adiçao
+  var adiçao = 1 + 1;
+  console.log(adiçao);
+
+  //subtraçao
+  var subtraçao = 1 - 1;
+  console.log(subtraçao);
+
+  //multiplicaçao
+  var multiplicaçao = 2 * 3;
+  console.log(multiplicaçao);
+
+  //divisao real
+  var divisaoReal = 6 / 2;
+  console.log(divisaoReal);
+
+  //divisao inteira
+  var divisaoInteira = 5 % 2;
+  console.log(divisaoInteira);
+
+  //potenciaçao
+  var potenciaçao = 2 ** 10;
+  console.log(potenciaçao);
+
+  //maior que
+  var maiorQue = 5 > 2;
+  console.log(maiorQue);
+
+  //maiorOuIguala
+  var maiorOuIguala = 5 >= 2;
+  console.log(maiorOuIguala);
+
+  
+
+  
---
+++ aula 3 - array e object/script.js
@@ -0,0 +1,9 @@
+// o que sao vetores ou arrays
+
+// como declarar um array
+/*let array = ['string', 1, true];
+console.log(array);*/
+
+//pode guardar varios tipos de dados
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log(array);
